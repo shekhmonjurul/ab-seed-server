ac326d07864972c3871de209baca2d8e87a3a440
shekhmonjurul <shekhmonjurul@gmail.com>
2025-10-03 16:44:27 +0600

    fix bug

--- src/modules/products/products.service.js
+++ src/modules/products/products.service.js
@@ -16,17 +16,16 @@ export const getProductsService = async () => {
     try {
         const woocom = await getWoocomConfig('products')
         const data = await woocom.json()
+        let productInfos = []
         if (woocom.ok) {
-            const productInfos = data.map((data) => {
+            productInfos = data.map((data) => {
                 const { name, short_description, sku, price, reguler_price, sale_price, stock_quantity, categories, images, stock_status } = data
                 return { name, short_description, sku, price, reguler_price, sale_price, stock_quantity, categories, images, stock_status }
             })
-        }else{
+        } else {
             return "woo-com api conection faild"
         }
 
-        const productsname = productInfos.map((productInfo) => productInfo.name)
-        console.log("product names: ", productInfos)
         return productInfos
     } catch (error) {
         console.log(error)
