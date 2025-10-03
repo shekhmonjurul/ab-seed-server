c8b1d975b0101f2e6b3fedd6ed543adcd0c87aa7
shekhmonjurul <shekhmonjurul@gmail.com>
2025-10-03 16:25:29 +0600

    security add

--- src/modules/products/products.service.js
+++ src/modules/products/products.service.js
@@ -1,13 +1,13 @@
-import {addWooComConfig, getWoocomConfig} from "../../config/woo-com/woo.com.config.js"
- 
+import { addWooComConfig, getWoocomConfig } from "../../config/woo-com/woo.com.config.js"
+
 export const addProductService = async (data) => {
     try {
-        
+
         const res = await addWooComConfig(data, "products")
         return await res.json()
-        
+
     } catch (error) {
-        
+
     }
 
 }
@@ -16,10 +16,15 @@ export const getProductsService = async () => {
     try {
         const woocom = await getWoocomConfig('products')
         const data = await woocom.json()
-        const productInfos = data.map((data)=>{
-            const {name, short_description, sku, price, reguler_price, sale_price, stock_quantity, categories, images, stock_status} = data
-            return {name, short_description, sku, price, reguler_price, sale_price, stock_quantity, categories, images, stock_status}
-    })
+        if (data.ok) {
+            const productInfos = data.map((data) => {
+                const { name, short_description, sku, price, reguler_price, sale_price, stock_quantity, categories, images, stock_status } = data
+                return { name, short_description, sku, price, reguler_price, sale_price, stock_quantity, categories, images, stock_status }
+            })
+        }else{
+            return "woo-com api conection faild"
+        }
+
         const productsname = productInfos.map((productInfo) => productInfo.name)
         console.log("product names: ", productInfos)
         return productInfos
