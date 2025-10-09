53e23c2b67cadf80dc6b44cc9ff78e4cf7b518c0
shekhmonjurul <shekhmonjurul@gmail.com>
2025-10-09 20:00:42 +0600

    config

--- src/config/steadfast/currier.log.database.js
+++ src/config/steadfast/currier.log.database.js
@@ -5,8 +5,8 @@ configDotenv()
 const { DB_STEDFAST_CURRIER, DB_HOST, DB_USER, DB_PASS} = process.env
 
 export const dbSteadfastConfig = {
-    steadfastdb:  DB_STEDFAST_CURRIER,
     host: DB_HOST,
     user: DB_USER,
-    password: DB_PASS
+    password: DB_PASS,
+    database:  DB_STEDFAST_CURRIER,
 }
