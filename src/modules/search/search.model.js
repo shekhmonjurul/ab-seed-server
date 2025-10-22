cf1079fae8aa957bc1f712837ba8305014007080
shekhmonjurul <shekhmonjurul@gmail.com>
2025-10-22 20:39:11 +0600

    ok bro

--- src/modules/search/search.model.js
+++ src/modules/search/search.model.js
@@ -1,7 +1,7 @@
 import db from "../../config/database/orders/orders.js"
 
 export const getFilterModel = async (filterword) => {
-    const sql = ` SELECT * FROM customers WHERE order_id LIKE '%${Number(filterword)}%'
+    const sql = ` SELECT * FROM orders WHERE order_id LIKE '%${Number(filterword)}%'
     OR number LIKE '%${filterword}%'`
     const [filterorders] = await db.query(sql)
     return filterorders
