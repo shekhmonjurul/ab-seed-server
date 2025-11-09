1c6257936057265c929bbe57c9ebb3711b420bcd
shekhmonjurul <shekhmonjurul@gmail.com>
2025-11-09 17:13:50 +0600

    push

--- src/modules/user/user.controller.js
+++ src/modules/user/user.controller.js
@@ -7,16 +7,26 @@ export let registrationController = async (req, res) => {
     let { name, password, role, employeId } = req.body;
     throwError(!name && !password && !role, "all fields required")
     let result = await registationServer(name, password, role, employeId)
-    response(res, {data : result})
+    response(res, { data: result })
 }
 
 export const loginController = async (req, res) => {
     let { password, employeeId } = req.body;
-    throwError( !password && !employeeId, 'name,employeId, password and role are required')
+    console.log("body: ", req.body)
+    throwError(!password && !employeeId, 'name,employeId, password and role are required')
     const token = await loginService(password, employeeId)
     throwError(!token, 'Invalid user id and password')
     console.log("token: ", token)
-    // res.cookies(token)
+    res.cookie(
+        "adminToken",
+        token,
+        {
+            httpOnly: true,
+            secure: false,
+            sameSite: 'lax',
+            maxAge: 60 * 60 * 1000,
+        }
+    );
     response(res, { data: token })
 }
 
