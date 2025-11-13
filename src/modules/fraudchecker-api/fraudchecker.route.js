a4c0ded319b72d93633cd42ae96b28e5e9401bcc
shekhmonjurul <shekhmonjurul@gmail.com>
2025-11-13 17:29:27 +0600

    add doc comment

--- src/modules/fraudchecker-api/fraudchecker.route.js
+++ src/modules/fraudchecker-api/fraudchecker.route.js
@@ -7,3 +7,5 @@ const router = Router();
 router.get("/", asyncHandler(addAndGetFraudController));
 
 export default router;
+
+// endpoint: get---> /api/fraudchecker?phone=01xxxxxxxxx  | find a phone number is fraud or no
